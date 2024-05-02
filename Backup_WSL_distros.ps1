20965d58aff55080a42e0d3271055cb6a7a28dec
Daniel Wituschek <daniel@wituschek.de>
2024-05-02 14:41:23 +0200

    Code structuring

--- Backup_WSL_distros.ps1
+++ Backup_WSL_distros.ps1
@@ -5,20 +5,21 @@
 $BackupDirectory = "C:\your\backup\path"
 
 # Frist check if network drive is available
-if (!(Test-Path $BackupDirectory)) {
-  Write-Host "Dir not accessibly! Is the drive connected?"
-  return
-}
+if (Test-Path $BackupDirectory) {
+  
+    # Get list of WSL distros
+    $distros = wsl --list --quiet
 
-# Get list of WSL distros
-$distros = wsl --list --quiet
+    Write-Host "WSL dristos found: $distros"
+    $confirmation = Read-Host "This will create a backup of each WSL distro and will take some time. Continue? "
 
-Write-Host "WSL dristos found: $distros"
-$confirmation = Read-Host "This will create a backup of each WSL distro and will take some time. Continue? "
-
-if ($confirmation -eq 'y') {
-  # Backup each distro
-  foreach ($distro in $distros) {
-    wsl --export $distro "$BackupDirectory\$distro.tar"
-  }
+    if ($confirmation -eq 'y') {
+        # Backup each distro
+        foreach ($distro in $distros) {
+            wsl --export $distro "$BackupDirectory\$distro.tar"
+        }
+    }
+} else {
+    Write-Host "Dir not accessibly! Is the drive connected?"
+    return
 }
